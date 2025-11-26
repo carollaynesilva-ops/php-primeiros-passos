34d4d4d1d47d16819fe17c5c0f08210dde5d60f1
carollaynesilva-ops <carollayne.silva@aluno.senai.br>
2025-11-26 14:55:30 -0300

    commit :)

--- ola.php
+++ ola.php
@@ -1,4 +1,20 @@
 <?php
-    echo "Olá mundo";
-    
+    echo "Olá mundo"; // é como um print, escreva
+    $nome= "Carollayne"; // criar uma variável
+    $sobrenome="Ferreira Da Silva";
+    define('MAX', 10);
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá mundo</title>
+</head>
+<body>
+    
+    <h1>OLÁ MUNDO, Tudo bem <?php echo $nome?> <?php echo $sobrenome?> </h1>
+
+</body>
+</html>
